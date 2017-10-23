54b39986a76c5160b4eb949d8e8eee85168aeab0
fernstgruber <fabianernstgruber@hotmail.com>
2017-10-23 11:57:34 +0200

    predict

--- scripts_2017/tryingtopredictparentmaterial.R
+++ scripts_2017/tryingtopredictparentmaterial.R
@@ -2,17 +2,17 @@ require(e1071)
 require(randomForest)
 require(rgdal)
 require(rgrass7)
-load("/media/fabs/Volume/01_PAPERZEUG/PROJECTP2/data2017/modeldata_sGUkartiert.RData")
-legend <- read.table("/media/fabs/Volume/01_PAPERZEUG/PROJECTP2/data2017/SGU_legend_new.txt",sep="\t",header=T)
+load("/home/fabs/PROJECTP2/data2017/modeldata_sGUkartiert.RData")
+legend <- read.table("/home/fabs/PROJECTP2/data2017/SGU_legend_new.txt",sep="\t",header=T)
 names(legend) <- c("SGU","SGUcode")
 dependent="SGU_kartiert"
 gisBase="/usr/local/src/grass70_release/dist.x86_64-unknown-linux-gnu"
-gisDbase =  "/media/fabs/Volume/Data/GRASSDATA/"
+gisDbase =  "/home/fabs/Data/GRASSDATA/"
 location="EPPAN_vhr"
 mapset="paper3data_predictparentmaterial"
 initGRASS(gisBase = gisBase,gisDbase = gisDbase,location=location,mapset=mapset,override = TRUE)
 
-predictors <- c("SGUcode","vectorruggedness_hr_ws51","TRI_hr_ws9")
+predictors <- c("SGUcode","vectorruggedness_hr_ws57","TRI_hr_ws31")
 modelcols <- c(dependent,predictors)
 SGU_gk <-readRAST("SGU")
 data <- SGU_gk@data
@@ -25,25 +25,24 @@ data$UID <- 1:nrow(data)
 names(data)
 modeldataoktober <- merge(modeldataoktober,legend,by.x="SGU_gk",by.y="SGU")
 modeldata <- modeldataoktober[c(modelcols)]
-modeldata <- merge(modeldata,legend,by.x="SGU_gk",by.y=)
 names(modeldata)
 modeldata$SGUcode <- factor(modeldata$SGUcode,levels=1:15)
 data$SGUcode <- factor(data$SGUcode,levels=1:15)
-str(data)
-str(modeldata)
 
-nadata <- na.omit(data)
-summary(nadata)
-#remove Mrd!!
+#remove Mrd!!???
 
 
 f <- paste(dependent,"~.")
 fit <- do.call("randomForest",list(as.formula(f),modeldata))
-preds <- predict(fit,newdata=data)
-preds <- predict(fit,newdata=data[,predictors])
-preds <- predict(fit, newdata=na.omit(data))
-preds <- predict(fit,newdata=as.matrix(data))
-preds <- predict(fit,newdata=nadata[predictors])
+
+data[["preds"]] <- predict(fit,newdata=data)
+load("datawithpreds_VRM57_TRI_31.RData")
+SGU_modell <- SGU_gk
+SGU_modell$UID <- 1:nrow(SGU_gk)
+load("datawithpreds_VRM57_TRI_31.RData")
+#SGU_modell <- merge(SGU_modell,data,by="UID",all.x=T) #hat die ganz nacht ned funktioniert
+summary(data)
+data <- data[order(data$UID,)]
 
 function(modeldata,dependent,predictors,legend,doreturn=FALSE,outname,input){
   require(e1071)
